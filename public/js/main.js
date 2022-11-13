7e671b5d775106cd29417bca398a36afec558081
Alima07 <alex.lima_dev@outlook.com>
2022-11-13 20:15:45 -0300

    Adicionando tarefas

--- public/js/main.js
+++ public/js/main.js
@@ -1,21 +1,79 @@
 window.onload = () =>{
+    
+    //--------------------------------------------------
+
     //CAPTURA DE ELEMENTOS
     
     //botao salvar
     const btnToSalve = document.getElementById('btn');
 
+    //Input type Text
+    const inputText = document.getElementById("text");
 
+    //
+    const list = document.getElementById('task-list');
 
     //--------------------------------------------------
 
-    //Definicao das Funcoes que serão executadas ( event handlers )
+    //DEFINICAO DAS FUNCOES QUE SERAO EXECUTADAS ( EVENT HANDLERS)
 
+    //botao salvar
     btnToSalve.addEventListener('click', (event) => {
 
+        event.preventDefault();
         
+        //1.Capturar o texto digitado no campo
+        let tarefa = inputText.value.toUpperCase();;
         
-    });
+        //--------------------------------------------------
+
+        //2.Criar um objeto tarefa: {texto: "Texto digitado", feito: false}
+        let objetoTarefa = [
+            
+            {
+                "texto": tarefa,
+                "feito": false,
+            }
+        ]
+
+        //--------------------------------------------------
+
+        //3.Adicionar esse objeto no array de tarefas;
+
+
+        //--------------------------------------------------
+        //4.Adicionar a tarefa na DOM
+
+        //4.1 Criar o novo li
+        let li = document.createElement('li');
+
+        //4.2 Adicionar o conteudo do li
+        li.innerHTML = 
+            `<input type="checkbox" id="check_1">
+            <label for="check_1"> ${tarefa} </label>
+            <i class="material-icons">delete</i>`
+
+        //4.3 Adicionar esse novo li na lista
+        list.appendChild(li);
+        
+        //4.4 Limpa o formulario com texto digitado
+        inputText.value = null;
+        //--------------------------------------------------
      
+    });
+    
+    //Input Text
+    inputText.addEventListener('keyup', (event) => {
+
+        // event.preventDefault();
+
+        // let id = event.target.id;
+        
+        // inputText.value = inputText.value.toUpperCase();
+
+        // console.log(inputText.value);
+
+    });
 
     //--------------------------------------------------
 }
