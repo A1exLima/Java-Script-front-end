f0785d1bcff858185d47b032ad1ea3203207c610
Alima07 <alex.lima_dev@outlook.com>
2022-11-13 16:50:34 -0300

    Primeiro commit

---
+++ public/js/main.js
@@ -0,0 +1,21 @@
+window.onload = () =>{
+    //CAPTURA DE ELEMENTOS
+    
+    //botao salvar
+    const btnToSalve = document.getElementById('btn');
+
+
+
+    //--------------------------------------------------
+
+    //Definicao das Funcoes que serão executadas ( event handlers )
+
+    btnToSalve.addEventListener('click', (event) => {
+
+        
+        
+    });
+     
+
+    //--------------------------------------------------
+}
