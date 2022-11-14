e523d050675e8debca3fb1b4e1e33c1057828922
Alima07 <alex.lima_dev@outlook.com>
2022-11-14 16:08:18 -0300

    Insercao de validacoes do formulario de preenchimento da tarefa

--- public/js/main.js
+++ public/js/main.js
@@ -10,7 +10,7 @@ window.onload = () =>{
     //Input type Text
     const inputText = document.getElementById("text");
 
-    //
+    //Lista de Tarefas UL
     const list = document.getElementById('task-list');
 
     //--------------------------------------------------
@@ -19,54 +19,94 @@ window.onload = () =>{
 
     //botao salvar
     btnToSalve.addEventListener('click', (event) => {
-
+        
         event.preventDefault();
         
         //1.Capturar o texto digitado no campo
-        let tarefa = inputText.value.toUpperCase();;
-        
-        //--------------------------------------------------
+        let tarefa = inputText.value.toUpperCase();
 
-        //2.Criar um objeto tarefa: {texto: "Texto digitado", feito: false}
-        let objetoTarefa = [
+        //1.0 Array de Erros
+        let errors = [];
+
+        //1.1 Verifica se o texto capturado possui algum valor
+        if(!tarefa){
             
-            {
-                "texto": tarefa,
-                "feito": false,
-            }
-        ]
+            inputText.style.border = "#3e6e6c dotted 2px";
+            errors.push('Preencha o campo');
+            
+        }
 
-        //--------------------------------------------------
+        //1.2 Captura o elemento ul onde exibira os erros
+        let ulErrors = document.querySelector('.errors ul');
 
-        //3.Adicionar esse objeto no array de tarefas;
+        //1.3 Cria um laço for para exibir os erros encontrado na array
+        for(let i = 0; i < errors.length; i++){
 
-
-        //--------------------------------------------------
-        //4.Adicionar a tarefa na DOM
-
-        //4.1 Criar o novo li
-        let li = document.createElement('li');
-
-        //4.2 Adicionar o conteudo do li
-        li.innerHTML = 
-            `<input type="checkbox" id="check_1">
-            <label for="check_1"> ${tarefa} </label>
-            <i class="material-icons">delete</i>`
-
-        //4.3 Adicionar esse novo li na lista
-        list.appendChild(li);
+            ulErrors.innerHTML += `<li> ${errors[i]} </li>`;
+        }
         
-        //4.4 Limpa o formulario com texto digitado
-        inputText.value = null;
-        //--------------------------------------------------
-     
+        //1.4 Verifica se existe algum erro, se sim inibi o envio do formulário ao clicar no botao salvar
+        if( errors.length > 0){
+
+            event.preventDefault();
+
+        }else {
+                    
+            // Limpa as mensagens de erros
+            ulErrors.innerHTML = ` `;
+            
+            //Formata a borda do formulario tarefa
+            inputText.style.border = "white solid 2px";
+
+            //--------------------------------------------------
+
+            //2.Criar um objeto tarefa: [ {"id": 1, "texto": "Texto digitado", "feito": false} ]
+            let objetoTarefa = 
+                
+                {
+                    "id": 1,
+                    "texto": tarefa,
+                    "feito": false
+                }
+            
+
+            //--------------------------------------------------
+
+            //3.Adicionar esse objeto no array de tarefas;
+
+            let arrayTarefas = [];
+
+            arrayTarefas.push(objetoTarefa);
+
+            console.log(arrayTarefas);
+
+            //--------------------------------------------------
+
+            //4.Adicionar a tarefa na DOM
+
+            //4.1 Criar o novo li
+            let li = document.createElement('li');
+
+            //4.2 Adicionar o conteudo do li
+            li.innerHTML = 
+                `<input type="checkbox" id="check_1">
+                <label for="check_1"> ${tarefa} </label>
+                <i class="material-icons">delete</i>`
+
+            //4.3 Adicionar esse novo li na lista
+            list.appendChild(li);
+            
+            //4.4 Limpa o formulario com texto digitado
+            inputText.value = null;
+            //--------------------------------------------------
+        }
     });
     
     //Input Text
     inputText.addEventListener('keyup', (event) => {
 
         // event.preventDefault();
-
+        
         // let id = event.target.id;
         
         // inputText.value = inputText.value.toUpperCase();
@@ -76,4 +116,8 @@ window.onload = () =>{
     });
 
     //--------------------------------------------------
+
 }
+
+
+    
